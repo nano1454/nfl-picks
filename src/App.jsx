af4787f56222f73531f740ebcb79643bee5a69dd
nano1454 <your-email@example.com>
2025-10-14 15:13:13 -0500

    Week 6 schedule update

--- src/App.jsx
+++ src/App.jsx
@@ -472,16 +472,7 @@ if (tbMissing !== -1) {
               Print / Save as PDF
             </button>
 
-            <Link to="/results">
-  <button
-       type="button"
-              style={printBtnStyle}
-              onMouseEnter={() => setResultsBtnStyle({ ...styles.btn, ...styles.btnHover })}
-              onMouseLeave={() => setResultsBtnStyle(styles.btn)}
-  >
-    View Picks Table
-  </button>
-</Link>
+ 
 
 
           </div>          
